33d0a1dccb16ef8ecc38b9123ee3dc567ff00c54
Vivek Wadher <122199360+vivekuw@users.noreply.github.com>
2025-12-27 11:55:20 +0530

    Initial commit

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route("/")
+def home():
+    return "🚀 CI/CD Success! Deployed via CodePipeline → Elastic Beanstalk"
+
+@application.route("/health")
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", port=8080)
